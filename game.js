19cf4dc7df96738473e9ae03dbf117a9c08e522a
fido2024 <fido.egd2020@gmail.com>
2026-04-06 16:29:18 -0400

    Canva: Dibujo mi lienzo color negro

--- game.js
+++ game.js
@@ -1,13 +1,6 @@
 const canvas = document.getElementById("gameCanvas");
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("2d"); //en que dimension
 
+// pinto y dibujo mi lienzo
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-ctx.fillStyle = "white";
-ctx.fillRect(250, 250, 100, 60);
-
-/**ctx.font = "20px Arial";
-ctx.fillStyle = "yellow";
-ctx.fillText("Hola me presento [SOY] Fidel", 200, 80);
-*/
