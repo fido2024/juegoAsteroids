5a073599112347366ea2826ef79eeac54f1faeef
fido2024 <fido.egd2020@gmail.com>
2026-04-06 16:49:59 -0400

    Canva: Dibujando rectangulos

--- game.js
+++ game.js
@@ -4,3 +4,11 @@ const ctx = canvas.getContext("2d"); //en que dimension
 // pinto y dibujo mi lienzo
 ctx.fillStyle = "black";
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+//dibujando rectangulos
+ctx.fillStyle = "white";
+ctx.fillRect(50, 50, 100, 60);
+
+ctx.fillStyle = "white";
+ctx.fillRect(250, 250, 200, 100);
+
