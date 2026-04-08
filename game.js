fcfb271a9d9135fec05b11d832e9e1d50562219d
fido2024 <fido.egd2020@gmail.com>
2026-04-08 11:55:32 -0400

    Dibujando mi nave un triangulo

--- game.js
+++ game.js
@@ -10,7 +10,7 @@ ctx.fillStyle = "white";
 ctx.fillRect(50, 50, 100, 60);
 
 ctx.fillStyle = "white";
-ctx.fillRect(200, 250, 200, 100);
+ctx.fillRect(200, 150, 200, 100);
 
 //se puede escribir texto en el canvas
 ctx.font = "20px Arial";
@@ -33,3 +33,22 @@ ctx.arc(500, 300, 60, 30, Math.PI);
 ctx.strokeStyle = "white";
 ctx.lineWidth = 6;
 ctx.stroke();
+
+//dibujando la nave un tringulo
+ctx.beginPath();
+ctx.moveTo(300, 300); 
+ctx.lineTo(330, 360); 
+ctx.lineTo(270, 360); 
+ctx.closePath();      // cierra la forma une el ultimo punto con el primero
+ctx.strokeStyle = "white";
+ctx.lineWidth = 2;
+ctx.stroke();
+
+ctx.beginPath();
+ctx.moveTo(400, 400);             
+ctx.lineTo(330 + 100, 360 + 100); 
+ctx.lineTo(270 + 100, 360 + 100); 
+ctx.closePath();
+ctx.strokeStyle = "white";
+ctx.lineWidth = 2;
+ctx.stroke()
